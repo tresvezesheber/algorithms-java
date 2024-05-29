5d54da20cb782951e5e3bc6496fcfe6e6be5c0d2
tresvezesheber <hebireaper@gmail.com>
2024-05-29 09:16:11 -0300

    feat(Anagram): add method isAnagramRecursive()

--- src/Anagram.java
+++ src/Anagram.java
@@ -14,26 +14,8 @@ public class Anagram {
             return false;
         }
 
-        HashMap<Character, Integer> stringMap1 = new HashMap<>();
-        HashMap<Character, Integer> stringMap2 = new HashMap<>();
-
-        for (int i = 0; i < string1.length(); i++) {
-            char character = string1.charAt(i);
-            if (stringMap1.containsKey(character)) {
-                stringMap1.put(character, stringMap1.get(character) + 1);
-            } else {
-                stringMap1.put(character, 1);
-            }
-        }
-
-        for (int i = 0; i < string2.length(); i++) {
-            char character = string2.charAt(i);
-            if (stringMap2.containsKey(character)) {
-                stringMap2.put(character, stringMap2.get(character) + 1);
-            } else {
-                stringMap2.put(character, 1);
-            }
-        }
+        HashMap<Character, Integer> stringMap1 = getMap(string1);
+        HashMap<Character, Integer> stringMap2 = getMap(string2);
 
         System.out.println(stringMap1);
         System.out.println(stringMap2);
@@ -50,5 +32,41 @@ public class Anagram {
         return true;
     }
 
+    public static HashMap<Character, Integer> getMap(String string) {
+        HashMap<Character, Integer> stringMap = new HashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            char character = string.charAt(i);
+            if (stringMap.containsKey(character)) {
+                stringMap.put(character, stringMap.get(character) + 1);
+            } else {
+                stringMap.put(character, 1);
+            }
+        }
+        return stringMap;
+    }
 
+    public static boolean isAnagramRecursive(String string1, String string2) {
+        string1 = string1.toLowerCase();
+        string2 = string2.toLowerCase();
+
+        if (string1.length() != string2.length()) {
+            return false;
+        }
+
+        if (string1.length() == 0) {
+            return true;
+        }
+
+        char firstChar = string1.charAt(0);
+        int indexInString2 = string2.indexOf(firstChar);
+
+        if (indexInString2 == -1) {
+            return false;
+        }
+
+        String newString1 = string1.substring(1);
+        String newString2 = string2.substring(0, indexInString2) + string2.substring(indexInString2 + 1);
+
+        return isAnagramRecursive(newString1, newString2);
+    }
 }
